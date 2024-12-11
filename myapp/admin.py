92caf5b211dbcb17a4a9c37e35cc8f67d6ca62d7
kelvintaii <7kelvintai@gmail.com>
2024-12-11 14:53:54 +0300

    add admin model for the member form

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Contact, Product
+from .models import Contact, Product, Member
 
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
@@ -12,3 +12,7 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'price', 'stock', 'created_at', 'updated_at')
     search_fields = ('name',)
     list_filter = ('created_at',)
+
+@admin.register(Member)
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'membership_type', 'joined_date')
